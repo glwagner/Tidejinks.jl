468ed2765d29f195ae0120bb36b0551733a36490
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-03 21:34:55 -0700

    Add testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,12 @@ using Tidejinks
 using Dates
 import SPICE
 
-t = DateTime(1993, 1, 1, 1) + Second(times[1])
+@testset "Test kernel wrangling..." begin
+    kernel_meta_file = "kernels.txt"
+    Tidejinks.wrangle_spice_kernels(kernel_meta_file)
+    SPICE.furnsh(kernel_meta_file)
+    @test isfile(kernel_meta_file)
+end
 
-kernel_meta_file = "kernels.txt"
-Tidejinks.wrangle_spice_kernels(kernel_meta_file)
-SPICE.furnsh(kernel_meta_file)
-
-@test isfile(kernel_meta_file)
+#    t = DateTime(1993, 1, 1, 1) + Second(times[1])
 
