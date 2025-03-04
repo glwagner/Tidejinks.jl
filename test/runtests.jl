a2040cf2b4d02db2b3a444eb55be5830e8b9f13d
Gregory Wagner <gregory.leclaire.wagner@gmail.com>
2025-03-03 21:45:22 -0700

    Add tidal potential test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using Tidejinks
 using Dates
+using Oceananigans
 import SPICE
 
 @testset "Test kernel wrangling..." begin
@@ -10,5 +11,13 @@ import SPICE
     @test isfile(kernel_meta_file)
 end
 
-#    t = DateTime(1993, 1, 1, 1) + Second(times[1])
+@testset "Computing tidal potentials..." begin
+    grid = LatitudeLongtiudeGrid(size=(90, 45, 1), longitude=(0, 360), latitude=(-90, 90), z=(0, 1))
+    Φ = Field{Center, Center, Nothing}(grid)
+    t = DateTime(1993, 1, 1, 1) + Second(times[1])
+    Tidejinks.compute_tidal_potential!(Φ, t)
+    @show maximum(Φ)
+    @show minimum(Φ)
+end
+
 
